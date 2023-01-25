d4eaa15b41e8a259c17160a71d8dfdb95c8b5ed5
Engelbert Vizcaya <engeldlgado@gmail.com>
2023-01-25 18:32:14 -0400

    FIX: Canonical URL for Blog and H1

--- pages/blog.js
+++ pages/blog.js
@@ -10,7 +10,7 @@ export default function Blog ({ posts }) {
       '@context': 'http://schema.org',
       '@type': 'WebSite',
       name: 'Blog de soluciones de desarrollo web y marketing | EngelDelgado',
-      url: 'https://engeldelgado.com',
+      url: 'https://engeldelgado.com/blog',
       description: 'Aprende a mejorar tu presencia en línea con nuestros artículos de desarrollo web y marketing. Explora nuestras soluciones innovadoras y conviértete en un experto en el tema.',
       image: 'https://engeldelgado.com/imagenes/logo.jpg'
     }
@@ -20,7 +20,7 @@ export default function Blog ({ posts }) {
       title='Blog de soluciones innovadoras de desarrollo web y marketing'
       description='Aprende a mejorar tu presencia en línea con nuestros artículos de desarrollo web y marketing. Explora nuestras soluciones innovadoras y conviértete en un experto en el tema.'
       ogType='website'
-      ogUrl='https://engeldelgado.com'
+      ogUrl='https://engeldelgado.com/blog'
       ogImage='https://engeldelgado.com/imagenes/Imagine-og.jpg'
       ogDescription='Aquí encontrarás contenido actualizado sobre soluciones de desarrollo web y marketing. Explora nuestros artículos y aprende cómo mejorar tu presencia en línea.'
       schemaObject={structureData}
@@ -29,10 +29,10 @@ export default function Blog ({ posts }) {
       <div className='relative py-16 bg-gray-50 dark:bg-base-100 sm:py-24 lg:py-24'>
         <div className='relative'>
           <div className='max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl'>
-            <h2 className='text-base font-semibold tracking-wider uppercase dark:text-primary text-secondary'>¡Aprende conmigo!</h2>
-            <p className='mt-2 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl'>
-              Recursos Útiles
-            </p>
+            <p className='text-base font-semibold tracking-wider uppercase dark:text-primary text-secondary'>¡Aprende conmigo!</p>
+            <h1 className='max-w-2xl mx-auto mt-2 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl'>
+              Nuestro blog de soluciones de desarrollo web y marketing
+            </h1>
             <p className='mx-auto mt-5 text-xl text-gray-500 dark:text-gray-50 max-w-prose'>
               Te mantendré actualizado sobre las últimas tendencias y te guiaré a través de cada paso para convertirte en un experto en el campo. ¡No te pierdas mi información valiosa y únete a mi comunidad de aprendizaje hoy!
             </p>
