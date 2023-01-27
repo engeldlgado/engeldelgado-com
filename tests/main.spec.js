97c9a3134d99e3cf608a61af90e6d45943055557
Engelbert Vizcaya <engeldlgado@gmail.com>
2023-01-27 10:27:47 -0400

    FIX: Test integrations

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -5,7 +5,7 @@ test.describe('Test if sitemap is generated and its correct', () => {
   test('Sitemap is generated', async ({ page }) => {
     await page.goto('/sitemap.xml')
     const sitemap = await page.textContent('urlset')
-    expect(sitemap).toContain('url')
+    expect(sitemap).toContain('http')
   })
 })
 
