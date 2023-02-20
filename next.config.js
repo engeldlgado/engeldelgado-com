98605561e044aca94be4d301391d067d51acedfb
Engelbert Vizcaya <engeldlgado@gmail.com>
2023-02-20 09:21:54 -0400

    UPDATE: XSS Security

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self' https: data: 'unsafe-inline' 'unsafe-eval'; script-src 'self' https: 'unsafe-inline' 'unsafe-eval'; style-src 'self' https: 'unsafe-inline'; img-src 'self' https: data:; media-src 'self' https: data:; frame-src 'self' https:; font-src 'self' https: data:; connect-src 'self' https: wss:; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'self';"
+  }
+]
+
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (isServer) {
@@ -10,6 +38,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com']
+  },
+  async headers () {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders
+      }
+    ]
   }
 }
 
